173729a7897beb63af7bbbdfcc7bb0c8a743e633
cdemkegit <craig.demke@utahtech.edu>
2024-02-28 16:12:25 -0700

    made some changes to the uth_clean_student_id. I also cleaned up the notes when I ran the check.

---
+++ R/globals.R
@@ -0,0 +1,14 @@
+# A list of global variables used in the package
+
+utils::globalVariables(c(
+  "student_id",
+  "count",
+  "outcome",
+  "outcome_missed",
+  "outcome_rate",
+  "outcome_rate_num",
+  "outcome_achieved",
+  "outcome_total",
+  "positive_outcome",
+  "."
+))
